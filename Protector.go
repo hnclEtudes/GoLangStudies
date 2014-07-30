b591bfc5b1668186b246c6657cb62c952f68e94d
H. Leung <scissorsmacgillicutty@gmail.com>
2014-07-30 00:28:49 -0400

    Yet untested version of the proxy server, although reviewing technique of channels as a gate mechanism w/instructor indicated this should work in principle (whether or not the implementation is correct is another story). Also: with the current scheme the error 503 will never be triggered.

--- Protector.go
+++ Protector.go
@@ -6,34 +6,49 @@ package main
  * five requests in and then turning others away with a 503
  * status unavilable error.
  *
- * Basics taken from https://gist.github.com/JalfResi/6287706
- * Thank you, Mr JalfResi!
+ * A bit more than based upon from https://gist.github.com/JalfResi/6287706
+ * Thank you sir!
  */
 import(
-        "log"
-        "net/url"
-        "net/http"
-        "net/http/httputil"
+	"log"
+	"net/url"
+	"net/http"
+	"net/http/httputil"
 )
 
-func main() {
-        remote, err := url.Parse("http://localhost:8181/")
-        if err != nil {
-                log.Fatalf("url.Parse of http://localhost:8181/ failed: %s\n", err)
-        }
-
-        proxy := httputil.NewSingleHostReverseProxy(remote)
-        http.HandleFunc("/", handler(proxy))
-        err = http.ListenAndServe(":8080", nil)
-        if err != nil {
-                panic(err)
-        }
-}
+const maxConcurrentRequests = 5
 
 func handler(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
-        return func(w http.ResponseWriter, r *http.Request) {
-                log.Println(r.URL)
-                w.Header().Set("X-Ben", "Rad")
-                p.ServeHTTP(w, r)
-        }
+	capacityQueue := make (chan int, maxConcurrentRequests)	
+	for i:= 0; i < maxConcurrentRequests; i++ {
+		capacityQueue <- i
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		// This two item read is the idiom in the language spec to check if the
+		// queue is empty (http://golang.org/ref/spec#Receive_operator)
+		// Regardless of type, if the queue is empty, it is assigned false
+		queueSlot, ok := <- capacityQueue 
+		if ok == false {
+			log.Printf("Client request for %s exceeds capacity of localhost:8181, returning 503 error\n", r.URL)
+			http.Error(w, "Service Unavailble (error 503): server capacity exceeded", http.StatusServiceUnavailable)
+		}
+		log.Printf("queueSlot %d, Client requested %s, forwarding to localhost:8181\n", queueSlot, r.URL)
+		p.ServeHTTP(w, r)
+		capacityQueue <- queueSlot
+	}
 }
+
+func main() {
+	remote, err := url.Parse("http://localhost:8181/")
+	if err != nil {
+		log.Fatalf("url.Parse of http://localhost:8181/ failed: %s\n", err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+	http.HandleFunc("/", handler(proxy))
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
