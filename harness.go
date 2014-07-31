5a03a40f11e7d5c955c9dbb911238fbef5ebc4b3
H. Leung <scissorsmacgillicutty@gmail.com>
2014-07-31 18:38:29 -0400

    Made makeRequestAndGetResponse a closure within another function, setupRequestAndReplyFunc (that also gets rid of globals, btw).

--- harness.go
+++ harness.go
@@ -16,9 +16,6 @@ var requestURL string	// This will be serverURL unless we find a variable in the
 						// to proxyURL, the reverse proxy made by Protector.go
 
 
-var numReqsFinished = 0
-var doneChannel = make(chan bool, 1)
-var semaphore = make(chan bool, 1)
 // Called as a goroutine. Note the semaphore around just the log.Printf outputs: 
 // we can let each routine talk to the server on its own, but only one routine
 // at a time can write to the log.
@@ -26,27 +23,35 @@ var semaphore = make(chan bool, 1)
 // internally how many are finished: when we reach maxRequest, we send true down
 // the doneChannel
 
-func makeRequestAndGetResponse (requestNumber int)  {
-	var myClient http.Client
+func setupRequestAndReplyFunc () (func (int), chan bool) {
+	var numReqsFinished = 0		// Counts the times makeRequestAndGetResponse returns:
+								// when it is equal to the number of times its called,
+								// we know all the goroutines are done.
+	var semaphore = make(chan bool, 1)	// Make sure one write to the log doesn't get clobbered by another
 
-	myRequest, _ := http.NewRequest("GET", requestURL, nil)
-	myResponse, _ := (&myClient).Do(myRequest)
-	lenContent, _ := strconv.Atoi(myResponse.Header.Get("Content-Length"))
-	theContent := make([]byte, lenContent)
-	actuallyRead, _ := myResponse.Body.Read(theContent)
-	myResponse.Body.Close()
-	semaphore <- true
-	log.Printf("Retrieval #%04d (Compare with server # in Content).\n", requestNumber)
-	log.Printf("\tResponse Status Code: %d\n", myResponse.StatusCode)
-	log.Printf("\tResponse Status '%s'\n", myResponse.Status)
-	log.Printf("\tLength: %d\n", actuallyRead)
-	log.Printf("\tResponse Content: %s\n", theContent)
-	numReqsFinished++
-	<- semaphore
-	if numReqsFinished == maxRequest {
-		doneChannel <- true
+	var doneChannel = make(chan bool, 1)	// Signals to other routine that all goroutines are done
+	makeRequestAndGetResponse := func (requestNumber int)  {
+		var myClient http.Client
+	
+		myRequest, _ := http.NewRequest("GET", requestURL, nil)
+		myResponse, _ := (&myClient).Do(myRequest)
+		lenContent, _ := strconv.Atoi(myResponse.Header.Get("Content-Length"))
+		theContent := make([]byte, lenContent)
+		actuallyRead, _ := myResponse.Body.Read(theContent)
+		myResponse.Body.Close()
+		semaphore <- true
+		log.Printf("Retrieval #%04d (Compare with server # in Content).\n", requestNumber)
+		log.Printf("\tResponse Status Code: %d\n", myResponse.StatusCode)
+		log.Printf("\tResponse Status '%s'\n", myResponse.Status)
+		log.Printf("\tLength: %d\n", actuallyRead)
+		log.Printf("\tResponse Content: %s\n", theContent)
+		numReqsFinished++
+		<- semaphore
+		if numReqsFinished == maxRequest {
+			doneChannel <- true
+		}
 	}
-	return
+	return makeRequestAndGetResponse, doneChannel
 }
 
 func main() {
@@ -61,6 +66,8 @@ func main() {
 	log.Printf("Informational messages print out from the routines makeRequest() and\n")
 	log.Printf("getResponse(). Both are run as go routines (although the second is gated\n")
 	log.Printf("to have but one instance running at a time)\n")
+
+	makeRequestAndGetResponse, doneChannel := setupRequestAndReplyFunc()
 
 	for i := 0; i < maxRequest; i++ {
 		go makeRequestAndGetResponse (i + 1)
