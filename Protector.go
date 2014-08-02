a7ef317984dfc711b335d26c90e6448277f83652
H. Leung <scissorsmacgillicutty@gmail.com>
2014-08-02 02:30:34 -0400

    Allow behavior for excess requests to be control by the values of an environment variable, PROTECTOR_OVERCAPACITY. If unset or PROTECTOR_OVERCAPACITY=200, then we let everything through (eventually, as one of the five slots frees up). If PROTECTOR_OVERCAPACITY=503, then we only let 75 at a time sit around (including what's being processed); more than that, and those requests get turned away immediately with error 503s (Service Unavailable) Note that since the harness.go (test client program) throws 100 requests at whichever server it's pointed at, we can count on certain numbers in tests, e.g., if PROTECTOR_OVERCAPACITY=503, 25 will have that return status, while 75 will be allowed in

--- Protector.go
+++ Protector.go
@@ -1,15 +1,17 @@
 package main
 
 /*
- * A reverse proxy for the server implemented by FiveOnlySrv.go
+ * A reverse proxy for the server implemented by SorryServer.go
  * (http://localhost:8181) that protects it by allowing only
- * five requests in and then turning others away with a 503
- * status unavilable error.
+ * five requests in and then, depending on the value of environment
+ * variable PROTECTOR_OVERCAPACITY, either discards them (when PROTECTOR_OVERCAPACITY
+ * is "503") with a 503 error or holding them until a slot opens up (PROTECTOR_OVERCAPACITY=200)
  *
  * A bit more than based upon from https://gist.github.com/JalfResi/6287706
  * Thank you sir!
  */
 import (
+	"os"
 	"log"
 	"net/http"
 	"net/http/httputil"
@@ -19,9 +21,13 @@ import (
 
 const maxConcurrentRequests = 5
 const maxReqsAllowed = 75
+const holdOvercapacityReqs = "200"
+const rejectOvercapacityReqs = "503"
 
-var totalReqs func() int // An awful global, so I can get the value out of the function
-// that calls the function that creates it and into main()
+var totalReqs func() int // A global, set within makeDefaultHandler, that returns the total
+// concurrent requests pending. I have to use a global; making it a return value would break
+// the footprint for handler function (and it is defined within a handler)
+// from the function that creates it.
 
 // makeTotalConcurrentCounters uses the methods Lock() and Unlock() on a *sync.Mutex to control
 // access to a counter that holds the number of concurrent requests the server is holding at
@@ -54,14 +60,14 @@ func makeTotalConcurrentCounters() (incrementTotal func(), decrementTotal func()
 	return
 }
 
-// This function actually isn't the handler but returns the handler, i.e. has a function
-// closure. This allows me to set up my Mutex secured increment and decrement functions
-// assemble an access queue so that only five of the concurrent requests are being processed
+// This function isn't the default handler but returns the default handler, i.e. has 
+// a function closure. This allows me to set up my Mutex secured increment and decrement functions
+// and assemble an access queue so that only five of the concurrent requests are being processed
 // at any time, and put the function that reports the number of requests in a a global,
 // so it may be called from main().
-func handler(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
-	capacityQueue := make(chan int, maxConcurrentRequests)
-	for i := 0; i < maxConcurrentRequests; i++ {
+func makeDefaultHandler(p *httputil.ReverseProxy, overcapacityBehavior string) func(http.ResponseWriter, *http.Request) {
+	capacityQueue := make(chan int, maxConcurrentRequests)	// the Brass Rings, as it were.
+	for i := 0; i < maxConcurrentRequests; i++ {			// grab one, and you're in. :)
 		capacityQueue <- i
 	}
 
@@ -71,11 +77,13 @@ func handler(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) 
 	return func(w http.ResponseWriter, r *http.Request) {
 		incrementConcurrentReqs() // do this BEFORE pulling off queue; we want to count the reqs that wait
 		log.Printf("New Request: now concurrent requests: %03d\n", tellMeNumReqs())
-		if tellMeNumReqs() >= maxReqsAllowed {
-			log.Printf("Surpassed capacity for %03d concurrent requests: sending back 503 error\n", maxReqsAllowed)
-			http.Error(w, "Maximum Capacity Exceeded", http.StatusServiceUnavailable)
-			return
-		}
+		if overcapacityBehavior == rejectOvercapacityReqs {	// Reject more than five concurrent requests
+			if tellMeNumReqs() > maxReqsAllowed {
+				log.Printf("Surpassed capacity for %03d concurrent requests: sending back 503 error\n", maxReqsAllowed)
+				http.Error(w, "Maximum Capacity Exceeded", http.StatusServiceUnavailable)
+				return
+			}
+		}	// overcapacityBehavior == rejectOvercapacityReqs
 		queueSlot := <-capacityQueue
 		log.Printf("queueSlot %d, Client requested %s, forwarding to localhost:8181 (totalReqs = %d)\n",
 			queueSlot, r.URL, tellMeNumReqs())
@@ -90,14 +98,42 @@ func haltsrv(w http.ResponseWriter, r *http.Request) {
 	log.Fatal("Stopping proxy server\n")
 }
 
+// initMsgs() gets the value of PROTECTOR_OVERCAPACITY from the environment and prints an appropriate
+// message for the value. Note that PROTECTOR_OVERCAPACITY doesn't need to be set; the default behavior
+// is to hold requests past five concurrent ones and let one in once a slot frees up.
+func initMsgs () (overcapacityBehavior string) {
+	overcapacityBehavior = os.Getenv("PROTECTOR_OVERCAPACITY")
+
+	switch {
+	case overcapacityBehavior == "": 
+		overcapacityBehavior = holdOvercapacityReqs // overcapacityBehavior == "200"
+		log.Printf("Reverse proxy default behavior: greater than %02d concurrent requests held until they\n", maxReqsAllowed)
+		log.Println("can be processed")
+    case overcapacityBehavior == holdOvercapacityReqs: // overcapacityBehavior == "200"
+		log.Printf("Reverse proxy will hold greater than %02d concurrent requests until they can be\n", maxReqsAllowed)
+		log.Println("processed. This is default behavior, i.e., the environment variable PROTECTOR_OVERCAPACITY")
+		log.Println("does not need to be set.")
+	case overcapacityBehavior == rejectOvercapacityReqs:	// overcapacityBehavior == "503"
+		log.Printf("Reverse proxy will discard with status 503 any concurrent requests greater than %02d\n", maxReqsAllowed)
+	default:
+		log.Printf("Warning: '%s' is not a legitimate value for PROTECTOR_OVERCAPACITY\n", overcapacityBehavior)
+		log.Println("PROTECTOR_OVERCAPACITY must either be (1) unset, (2) '200', or '503'")
+		log.Println("Default behavior (PROTECTOR_OVERCAPACITY is unset) is the same as PROTECTOR_OVERCAPACITY = '200', i.e.")
+		log.Println("if more than five concurrent requests are pending, they just wait until a slot opens up.")
+		log.Fatal("Aborting.")
+	}
+	return overcapacityBehavior
+}
+
 func main() {
 	remote, err := url.Parse("http://localhost:8181/")
 	if err != nil {
 		log.Fatalf("url.Parse of http://localhost:8181/ failed: %s\n", err)
 	}
+	overcapacityBehavior := initMsgs()
 
 	proxy := httputil.NewSingleHostReverseProxy(remote)
-	http.HandleFunc("/", handler(proxy))
+	http.HandleFunc("/", makeDefaultHandler(proxy, overcapacityBehavior))
 	http.HandleFunc("/HALT", haltsrv)
 	log.Printf("Reverse proxy to http://localhost:8181 about to accept requests at http://locahost:8080\n")
 	err = http.ListenAndServe(":8080", nil)
